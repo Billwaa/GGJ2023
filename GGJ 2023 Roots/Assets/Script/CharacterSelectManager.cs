115bf7d549cb407cab508302d8cc7e8da116abb4
walter <walterchoi0131@gmail.com>
2023-02-02 22:29:37 +0800

    chracter select manager - script update

--- GGJ 2023 Roots/Assets/Script/CharacterSelectManager.cs
+++ GGJ 2023 Roots/Assets/Script/CharacterSelectManager.cs
@@ -1,35 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CharacterSelectManager : MonoBehaviour
 {
     public GameObject[] characterSelections;
-
+    public GameObject errorText;
     void BeginGameCheck(){
 
         bool steadyGo = true;
         int playerNumber = 0;
         for (int i = 0; i < characterSelections.Length; i++)
         {
-            if (characterSelections[0].GetComponent<CharacterSelect>().getReadyStatus()==readyStatus.SELECTING){
+            if (characterSelections[i].GetComponent<CharacterSelect>().getReadyStatus()==readyStatus.SELECTING){
                 steadyGo = false;
                 break;
             }
         }
 
         if (!steadyGo){
+        errorText.SetActive(true);
+        errorText.GetComponent<TextMeshProUGUI>().text="Someone is not ready!";
         Debug.Log("還有人未準備好!");
         return;
         }
 
 
         for (int i = 0; i < characterSelections.Length; i++){
-            if (characterSelections[0].GetComponent<CharacterSelect>().getReadyStatus()==readyStatus.RETIRED){
+            if (characterSelections[i].GetComponent<CharacterSelect>().getReadyStatus()==readyStatus.RETIRED){
                 PlayerPrefs.SetString("P"+i.ToString()+"joinStatus","false");
             }
             
-            if (characterSelections[0].GetComponent<CharacterSelect>().getReadyStatus()==readyStatus.READY){
+            if (characterSelections[i].GetComponent<CharacterSelect>().getReadyStatus()==readyStatus.READY){
                 playerNumber +=1;
                 PlayerPrefs.SetString("P"+i.ToString()+"joinStatus","true");
                 PlayerPrefs.SetInt("P"+i.ToString()+"selectedCharacter",characterSelections[0].GetComponent<CharacterSelect>().getCharacterSelected());
@@ -37,17 +40,19 @@ public class CharacterSelectManager : MonoBehaviour
         }
 
         if (playerNumber<2){
+        errorText.SetActive(true);
+        errorText.GetComponent<TextMeshProUGUI>().text="Atleast 2 players are required!";
         Debug.Log("至少需要2名玩家!");
         return;
         }
 
-        Debug.Log("觸發開始遊戲");
+        Debug.Log("觸發開始遊戲..."+playerNumber.ToString()+"人玩");
         BeginGame();
 
     }
 
     void BeginGame(){
-
+        errorText.SetActive(false);
         Debug.Log("開始遊戲");
 
     }
