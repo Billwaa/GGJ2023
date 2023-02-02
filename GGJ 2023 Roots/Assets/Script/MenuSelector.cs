00b4fa79eea5ab067ae1dfa00a2057b50f183b63
gordonkwokkwok <gordonhei25@gmail.com>
2023-02-02 11:08:35 +0800

    Added Script - MenuSelector

---
+++ GGJ 2023 Roots/Assets/Script/MenuSelector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSelector : MonoBehaviour
+{
+    public void selectScene()
+    {
+        switch (this.gameObject.name)
+        {
+            case "PlayButton":
+                SceneManager.LoadScene("CharacterSelectScene");
+                break;
+            case "GalleryButton":
+                SceneManager.LoadScene("CharacterGalleryScene");
+                break;
+            case "QuitGameButton":
+                Debug.Log("Quit");
+                Application.Quit();
+                break;
+        }
+    }
+}
