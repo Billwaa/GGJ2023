c0f3512124dd11506e77f6f8bdcaaae153081c47
louischoi123 <bchoi1997123@gmail.com>
2023-02-03 22:35:44 +0800

    Add GameStartButton,QuitButton

---
+++ GGJ 2023 Roots/Assets/StartGameButton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameButton : MonoBehaviour
+{
+    // Bulld number of scene to start when start button is pressed
+    public int gameStartScene;
+
+
+    // Update is called once per frame
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameStartScene);
+
+    }
+}
